8a9a39a3dfd8b13e5edc1c130a67e94e71a66b34
Joe Downes-Berry <j.d.berry26@gmail.com>
2021-01-14 14:05:37 -0500

    commit tree 95a6a1bbec15b644cfec5011ec85505f2bf98896

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |attendees|
+        new_array << badge_maker(attendees)
+    end
+    new_array
+end
+
+def assign_rooms(array)
+    new_array = []
+    array.each.with_index(1) do |name, index|
+        new_array << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    new_array
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each {|badge| puts badge}
+    assign_rooms(attendees).each{|rooms| puts rooms}
+end  
